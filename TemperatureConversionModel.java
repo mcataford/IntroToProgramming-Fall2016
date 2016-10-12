95ab051e8d564938a0e8aca4a48c55846162e430
Marc Cataford <mcataford@users.noreply.github.com>
2016-10-11 20:59:19 -0400

    Add files via upload

---
+++ TemperatureConversionModel.java
@@ -0,0 +1,25 @@
+/*
+*   TemperatureConversionModel.java
+*   @author: Marc Cataford
+*/
+
+public class TemperatureConversionModel {
+    public static void main(String[] args) {
+        //degreesCelsius contains the temperature to convert.
+        //Q: Why do we define this as a 'double'?
+        //You need to give this a value to be able to test the conversion!
+        double degreesCelsius;
+
+        /*
+        *   Insert your code here
+        */
+
+        //degreesFahrenheit contains the converted temperature.
+        //You need to assign this the result of the conversion!
+        double degreesFahrenheit;
+
+        /*
+        *   Insert the code to print your results here!
+        */
+    }
+}
